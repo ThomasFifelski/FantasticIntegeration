569f2a3957bd0bf39d29a4effa9e2f30d6d257ab
Tom Fifelski <thomasfifelski@gmail.com>
2013-09-06 15:58:46 -0500

    we dont need build folder

--- FantasticIntegeration/src/main/java/com/delta/fantasticintegeration/MainActivity.java
+++ FantasticIntegeration/src/main/java/com/delta/fantasticintegeration/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         Log.i("hey", "yup");
+        Log.i("skdfhsdkjf", "slkdfhskldfj");
     }
 
 
